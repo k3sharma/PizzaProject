28a50ff8356410d9eb6ce69c5ac61f2b10560cb0
k3sharma <112501728+k3sharma@users.noreply.github.com>
2022-11-29 11:50:02 -0500

    Add files via upload

---
+++ HelpLoginPageController.java
@@ -0,0 +1,28 @@
+package com.example.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelpLoginPageController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    
+    public void start(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("/FXMLfiles/HelpLoginPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        
+    }
+}
---
+++ TypeOfDrinkController.java
@@ -0,0 +1,26 @@
+package com.example.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TypeOfDrinkController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+    public void start(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/FXMLfiles/TypeOfDrink.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+}
